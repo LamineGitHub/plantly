01df12c35d554e7379cd63ad8ea2021aa1c4fce6
Lamine Diallo <98341361+LamineGitHub@users.noreply.github.com>
2025-02-11 01:15:28 +0000

    Add initial project setup with Expo, Tailwind CSS, and essential assets

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import { theme } from "./src/theme";
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        colorGreen: theme.colorGreen,
+        colorLeafyGreen: theme.colorLeafyGreen,
+        colorAppleGreen: theme.colorAppleGreen,
+        colorLimeGreen: theme.colorLimeGreen,
+        colorLightGrey: theme.colorLightGrey,
+      },
+      fontFamily: {
+        "inter-bold": ["Inter-bold", "sans-serif"],
+        "inter-semiBold": ["Inter-semibold", "sans-serif"],
+        "inter-medium": ["Inter-medium", "sans-serif"],
+        inter: ["Inter-regular", "sans-serif"],
+        caveat: ["Caveat", "sans-serif"],
+      },
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
