4abad501929ebd3d973478f1b4bd0f3fcfd82533
Lamine Diallo <98341361+LamineGitHub@users.noreply.github.com>
2025-02-13 17:46:39 +0000

    feat: Make PlantCard clickable to navigate to plant details

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,19 +1,22 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 import { PlantType } from "@/types";
 import { PlantlyImage } from "@/components/PlantlyImage";
+import { Link } from "expo-router";
 
 export default function PlantCard({ plant }: { plant: PlantType }) {
   return (
-    <View className="mb-3 flex-row rounded-md bg-white p-3 shadow-md shadow-black/60">
-      <PlantlyImage size={100} imageUri={plant.imageUri} />
-      <View className="justify-center p-[14px]">
-        <Text numberOfLines={1} className="mb-1 font-inter-bold text-lg">
-          {plant.name}
-        </Text>
-        <Text className="font-inter text-slate-500">
-          Water every {plant.wateringFrequencyDays} days
-        </Text>
-      </View>
-    </View>
+    <Link href={`/plant/${plant.id}`} asChild>
+      <Pressable className="mb-3 flex-row rounded-md bg-white p-3 shadow-md shadow-black/60">
+        <PlantlyImage size={100} imageUri={plant.imageUri} />
+        <View className="justify-center p-[14px]">
+          <Text numberOfLines={1} className="mb-1 font-inter-bold text-lg">
+            {plant.name}
+          </Text>
+          <Text className="font-inter text-slate-500">
+            Water every {plant.wateringFrequencyDays} days
+          </Text>
+        </View>
+      </Pressable>
+    </Link>
   );
 }
